f802b5dc0a5089e2e6e3a9fb0de94e4256c784a6
Lucas Martin-King <lmartinking@gmail.com>
2025-06-24 20:56:38 +1000

    WIP code

---
+++ src/cpu.rs
@@ -0,0 +1,122 @@
+use std::fmt;
+use std::fs;
+use std::cmp;
+
+use nix::unistd;
+
+use colorsys::{Rgb, Hsl};
+
+
+#[derive(Debug, PartialEq)]
+pub enum CPUAppletError {
+    CPUCountError,
+    CPUInfoError,
+}
+
+impl std::error::Error for CPUAppletError {}
+
+impl fmt::Display for CPUAppletError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "CPUAppletError: {:?}", self)
+    }
+}
+
+type Result<T> = std::result::Result<T, CPUAppletError>;
+
+pub fn cpu_count() -> Result<u32>
+{
+    let count = match unistd::sysconf(unistd::SysconfVar::_NPROCESSORS_ONLN) {
+        Ok(Some(c)) => c as u32,
+        Ok(None) => return Err(CPUAppletError::CPUCountError),
+        Err(_) => return Err(CPUAppletError::CPUCountError),
+    };
+    Ok(count)
+}
+
+#[derive(Debug, PartialEq)]
+pub struct CPUInfo {
+    pub min_freq: u32,
+    pub max_freq: u32,
+    pub cur_freq: u32,
+}
+
+fn read_u32_from_file(path: &str) -> Option<u32>
+{
+    let vstr = match fs::read_to_string(path) {
+        Ok(v) => v,
+        Err(_) => return None
+    };
+
+    let vstr = vstr.trim_end();
+
+    match vstr.parse::<u32>() {
+        Ok(v) => Some(v),
+        Err(_) => return None
+    }
+}
+
+fn normalise_cur_freq(cpu: &CPUInfo) -> f32 {
+    let mut c = cpu.cur_freq;
+    if c < cpu.min_freq { c = cpu.min_freq }
+    if c > cpu.max_freq { c = cpu.max_freq }
+    let range = cpu.max_freq - cpu.min_freq;
+    let adj = c - cpu.min_freq;
+    return adj as f32 / range as f32
+}
+
+fn cpu_info(cpu_index: u32) -> Result<CPUInfo>
+{
+    let min_freq_path = format!("/sys/bus/cpu/devices/cpu{cpu_index}/cpufreq/scaling_min_freq");
+    let max_freq_path = format!("/sys/bus/cpu/devices/cpu{cpu_index}/cpufreq/scaling_max_freq");
+    let cur_freq_path = format!("/sys/bus/cpu/devices/cpu{cpu_index}/cpufreq/scaling_cur_freq");
+
+    match (read_u32_from_file(&min_freq_path), read_u32_from_file(&max_freq_path), read_u32_from_file(&cur_freq_path)) {
+        (Some(min), Some(max), Some(cur)) => Ok(CPUInfo { min_freq: min, max_freq: max, cur_freq: cur }),
+        _ => Err(CPUAppletError::CPUInfoError)
+    }
+}
+
+impl fmt::Display for CPUInfo {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "CPUInfo: {:?}", self)
+    }
+}
+
+pub fn applet(_: &[String]) -> Result<()> {
+    let c = cpu_count()?;
+
+    for i in 0..c {
+        let info = cpu_info(i)?;
+        let norm = normalise_cur_freq(&info) * 100.0;
+        println!("CPU {i} Info: {info} Norm: {norm:.0}%");
+    }
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_hsl_to_rgb_red() {
+        let hsl = Hsl::from((0.0, 100.0, 50.0));
+        let rgb: [u8; 3] = Rgb::from(&hsl).into();
+        assert_eq!((255, 0, 0), rgb.into());
+    }
+
+    #[test]
+    fn test_hsl_to_rgb_green() {
+        let hsl = Hsl::from((120.0, 100.0, 50.0));
+        let rgb: [u8; 3] = Rgb::from(&hsl).into();
+        assert_eq!((0, 255, 0), rgb.into());
+    }
+
+    #[test]
+    fn test_normalise_cur_freq() {
+        assert_eq!(0.0, normalise_cur_freq(&CPUInfo { min_freq: 100, max_freq: 200, cur_freq: 99 }));
+        assert_eq!(0.0, normalise_cur_freq(&CPUInfo { min_freq: 100, max_freq: 200, cur_freq: 100 }));
+        assert_eq!(0.5, normalise_cur_freq(&CPUInfo { min_freq: 100, max_freq: 200, cur_freq: 150 }));
+        assert_eq!(1.0, normalise_cur_freq(&CPUInfo { min_freq: 100, max_freq: 200, cur_freq: 201 }));
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,12 @@
-use std::fmt;
-use std::fs;
+use std::{env, fmt};
 
-use nix::unistd;
+use tmux_applets::cpu;
 
-#[derive(Debug, PartialEq)]
-pub enum AppletError {
-    CPUCountError,
-    CPUInfoError,
+#[derive(Debug)]
+enum AppletError {
+    CPUAppletError(cpu::CPUAppletError),
+    MissingArgumentError,
 }
-
-impl std::error::Error for AppletError {}
 
 impl fmt::Display for AppletError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
@@ -17,65 +14,35 @@ impl fmt::Display for AppletError {
     }
 }
 
+impl From<cpu::CPUAppletError> for AppletError {
+    fn from(error: cpu::CPUAppletError) -> Self {
+        AppletError::CPUAppletError(error)
+    }
+}
+
+impl std::error::Error for AppletError {}
+
 type Result<T> = std::result::Result<T, AppletError>;
 
-fn cpu_count() -> Result<u32>
-{
-    let count = match unistd::sysconf(unistd::SysconfVar::_NPROCESSORS_ONLN) {
-        Ok(Some(c)) => c as u32,
-        Ok(None) => return Err(AppletError::CPUCountError),
-        Err(_) => return Err(AppletError::CPUCountError),
-    };
-    Ok(count)
-}
+const USAGE: &'static str = "
+usage: tmux-applets <applet> [<args>]
 
-#[derive(Debug, PartialEq)]
-pub struct CPUInfo {
-    pub min_freq: u32,
-    pub max_freq: u32,
-    pub cur_freq: u32,
-}
+available applets:
 
-fn read_u32_from_file(path: &str) -> Option<u32>
-{
-    let vstr = match fs::read_to_string(path) {
-        Ok(v) => v,
-        Err(_) => return None
-    };
-
-    let vstr = vstr.trim_end();
-
-    match vstr.parse::<u32>() {
-        Ok(v) => Some(v),
-        Err(_) => return None
-    }
-}
-
-fn cpu_info(cpu_index: u32) -> Result<CPUInfo>
-{
-    let min_freq_path = format!("/sys/bus/cpu/devices/cpu{cpu_index}/cpufreq/scaling_min_freq");
-    let max_freq_path = format!("/sys/bus/cpu/devices/cpu{cpu_index}/cpufreq/scaling_max_freq");
-    let cur_freq_path = format!("/sys/bus/cpu/devices/cpu{cpu_index}/cpufreq/scaling_cur_freq");
-
-    match (read_u32_from_file(&min_freq_path), read_u32_from_file(&max_freq_path), read_u32_from_file(&cur_freq_path)) {
-        (Some(min), Some(max), Some(cur)) => Ok(CPUInfo { min_freq: min, max_freq: max, cur_freq: cur }),
-        _ => Err(AppletError::CPUInfoError)
-    }
-}
-
-impl fmt::Display for CPUInfo {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "CPUInfo: {:?}", self)
-    }
-}
+    cpu: show cpu frequency usage
+";
 
 fn main() -> Result<()> {
-    let c = cpu_count()?;
+    let args: Vec<String> = env::args().collect();
 
-    for i in 0..c {
-        let info = cpu_info(i)?;
-        println!("CPU {i} Info: {info}");
+    if args.len() == 1 {
+        println!("{USAGE}");
+        return Err(AppletError::MissingArgumentError);
     }
 
-    Ok(())
+    match args[1].as_str() {
+        "-h" | "--help" | "help" => { println!("{USAGE}"); return Ok(()) }
+        "cpu" => { Ok(cpu::applet(&args[2..])?) }
+        _ => { println!("{USAGE}"); return Err(AppletError::MissingArgumentError) }
+    }
 }
