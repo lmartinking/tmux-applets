7b67addf459375916cbaed41ffe248fe50b28f2c
Lucas Martin-King <lmartinking@gmail.com>
2025-06-24 22:09:23 +1000

    WIP get it working

--- src/cpu.rs
+++ src/cpu.rs
@@ -55,6 +55,7 @@ fn read_u32_from_file(path: &str) -> Option<u32>
     }
 }
 
+// convert the current cpu frequency into a percentage, range [0.0, 1.0]
 fn normalise_cur_freq(cpu: &CPUInfo) -> f32 {
     let mut c = cpu.cur_freq;
     if c < cpu.min_freq { c = cpu.min_freq }
@@ -62,6 +63,21 @@ fn normalise_cur_freq(cpu: &CPUInfo) -> f32 {
     let range = cpu.max_freq - cpu.min_freq;
     let adj = c - cpu.min_freq;
     return adj as f32 / range as f32
+}
+
+fn pct_to_hue(pct: f32) -> f32 {
+    const LEFT_STOP: f32 = 0.0; // Red
+    const RIGHT_STOP: f32 = 120.0; // Green
+    assert!(RIGHT_STOP > LEFT_STOP);
+    // Note: pct is inverted as we want to transition from green to red
+    LEFT_STOP + ((1.0 - pct) * (RIGHT_STOP - LEFT_STOP))
+}
+
+fn cpu_freq_hsl(norm: f32, s: Option<f32>, l: Option<f32>) -> Hsl {
+    let hue = pct_to_hue(norm);
+    let s = s.unwrap_or(100.0);
+    let l = l.unwrap_or(50.0);
+    Hsl::from((hue, s, l))
 }
 
 fn cpu_info(cpu_index: u32) -> Result<CPUInfo>
@@ -88,8 +104,15 @@ pub fn applet(_: &[String]) -> Result<()> {
     for i in 0..c {
         let info = cpu_info(i)?;
         let norm = normalise_cur_freq(&info) * 100.0;
-        println!("CPU {i} Info: {info} Norm: {norm:.0}%");
+
+        let c = cpu_freq_hsl(norm, None, None);
+        let rgb = Rgb::from(&c);
+
+        eprintln!("CPU {i} Info: {info} Norm: {norm:.0}% RGB: {}", rgb.to_hex_string());
+
+        print!("#[bg={}]  ", rgb.to_hex_string());
     }
+    println!("#[default]");
 
     Ok(())
 }
