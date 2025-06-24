ad26b283b52dfd93df3e3bff712f9be171a9c6f6
Lucas Martin-King <lmartinking@gmail.com>
2025-06-24 23:02:56 +1000

    Update usage

--- src/main.rs
+++ src/main.rs
@@ -30,6 +30,10 @@ usage: tmux-applets <applet> [<args>]
 available applets:
 
     cpu: show cpu frequency usage
+
+        optional parameters:
+          s:XX.YY set the saturation (eg: s:50.0)
+          l:XX.YY set the lightness  (eg: l:75.0)
 ";
 
 fn main() -> Result<()> {
